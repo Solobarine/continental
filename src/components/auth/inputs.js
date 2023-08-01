b7822a09fbc78178ddae380e9b5acb140627da97
Solly <solobarine@gmail.com>
2023-08-01 06:53:31 +0100

    Add Input Data for Login and Register

---
+++ src/components/auth/inputs.js
@@ -0,0 +1,70 @@
+const login_inputs = [
+    {
+        label: 'Enter Your Email',
+        type: 'email',
+        name: 'email',
+        autocomplete: 'email',
+        required: true,
+        error_message: 'Enter a valid Email'
+    },
+    {
+        label: 'Enter Your Password',
+        type: 'password',
+        name: 'password',
+        pattern: "^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,20}$",
+        autocomplete: 'password',
+        required: true,
+        error_message: 'Password should be 8-20 characters long with at least 1 letter and 1 number'
+    }
+]
+
+const register_inputs = [
+    {
+        label: 'First Name',
+        type: 'text',
+        name: 'first_name',
+        pattern: '^[A-Za-z]{3,16}$',
+        autocomplete: 'first_name',
+        required: true,
+        error_message: 'First Name should be 3-16 characters long with no special characters',
+    },
+    {
+        label: 'Last Name',
+        type: 'text',
+        name: 'last_name',
+        pattern: '^[A-Za-z]{3,16}$',
+        autocomplete: 'last_name',
+        required: true,
+        error_message: 'Last Name should be 3-16 characters long with no special characters',
+    },
+    {
+        label: 'Enter Your Email',
+        type: 'email',
+        name: 'email',
+        pattern: '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',
+        autocomplete: 'email',
+        required: true,
+        error_message: 'Enter a valid Email'
+    },
+    {
+        label: 'Enter Your Password',
+        type: 'password',
+        name: 'password',
+        pattern: "^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,20}$",
+        autocomplete: 'password',
+        required: true,
+        error_message: 'Password should be 8-20 characters long with at least 1 letter and 1 number'
+    },
+    {
+        label: 'Confirm Password',
+        type: 'password',
+        name: 'confirm_password',
+        pattern: "^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,20}$",
+        autocomplete: 'password',
+        required: true,
+        error_message: 'Password should be 8-20 characters long with at least 1 letter and 1 number'
+    }
+
+]
+
+export { login_inputs, register_inputs }
