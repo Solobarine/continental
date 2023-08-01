0caf3cb3cf1a8fab812eae9c498cfc82ca37be8b
Solly <solobarine@gmail.com>
2023-08-01 07:07:49 +0100

    Async Loading of Route Window Component

---
+++ src/components/layout/Parent.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <Suspense>
+      <template #default>
+        <RouteWindow />
+      </template>
+      <template #fallback>
+        <Loading />
+      </template>
+    </Suspense>
+  </section>
+</template>
+
+<script setup>
+import Loading from '../reusables/Loading.vue'
+import RouteWindow from './RouteWindow.vue'
+</script>
+<style>
+#parent {
+  position: relative;
+  display: flex;
+  gap: 20px;
+}
+
+.route {
+  flex-grow: 1;
+}
+
+@media screen and (max-width: 650px) {
+  #parent {
+    padding-right: 20px;
+    padding-bottom: 20px;
+  }
+
+  .route {
+    padding-left: 55px;
+  }
+}
+</style>
