12914debd2d92ced417621ea6e3181e37178574d
Solly <solobarine@gmail.com>
2023-08-01 07:13:21 +0100

    Create Reusable Components

---
+++ src/components/reusables/Card.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="card">
+    <div>
+      <Chip />
+      <Visa />
+    </div>
+    <p id="name">{{ first_name }} {{ last_name }}</p>
+    <strong>123 XXX XXX 345</strong>
+    <p id="company">Continental</p>
+  </div>
+</template>
+<script setup>
+import Visa from "./Visa.vue";
+import Chip from "./Chip.vue";
+
+const props = defineProps({
+  first_name: String,
+  last_name: String,
+});
+</script>
+<style scoped>
+#card {
+  width: 250px;
+  padding: 10px;
+  background-color: blueviolet;
+  border-radius: 20px;
+}
+
+#card > div {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+svg {
+  display: block;
+}
+#card #chip {
+  width: 60px;
+}
+
+#card #visa {
+  width: 50px;
+  fill: var(--text);
+}
+
+#name {
+  margin-top: 10px;
+  margin-bottom: 5px;
+}
+
+strong {
+  display: block;
+  text-align: center;
+  font-size: 1.2rem;
+  text-align: center;
+  margin-bottom: 5px;
+}
+
+#company {
+  font-size: 26px;
+  font-family: "Great Vibes", serif;
+  text-align: center;
+}
+</style>
---
+++ src/components/reusables/Transaction.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="transaction">
+    <div>
+      <img src="/src/assets/female_user.png" alt="arya" />
+      <p>A. Devin</p>
+    </div>
+    <small class="status">completed</small>
+    <small>20 Jun 2023</small>
+    <p>$1456</p>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  name: String,
+  image: String,
+  status: String,
+  date: Date,
+  amount: Number,
+});
+</script>
+<style scoped>
+#transaction {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 3px;
+  margin-bottom: 5px;
+}
+
+#transaction > div {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+#transaction > div p {
+  font-size: 0.8rem;
+}
+
+img {
+  width: 35px;
+  display: block;
+}
+
+.status {
+  background-color: rgb(165, 231, 209);
+  color: var(--green);
+  padding: 8px;
+  border-radius: 12px;
+}
+</style>
