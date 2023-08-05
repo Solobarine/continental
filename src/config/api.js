18fa5aa90fc72a882eb662d8e26c049dce688632
Solly <solobarine@gmail.com>
2023-08-05 08:38:42 +0100

    Update Config File

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-const status = 'development'
+const status = 'production'
 
-const domain = (status === 'development') ? 'http://localhost:8000/api' : 'https:whatever.com'
+const domain = (status === 'development') ? 'http://localhost:8000/api' : 'https://morning-refuge-28092-b630b51cff9f.herokuapp.com/api'
 
 export default domain
