abd5ba087ecb07a8724a6dcc0a298dcaaf32cea6
Solly <solobarine@gmail.com>
2023-08-10 08:44:24 +0100

    Update BAckend URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const status = 'development'
 
-const domain = (status === 'development') ? 'http://localhost:8000/api' : 'https:whatever.com'
+const domain = (status === 'development') ? 'http://localhost:8000/api' : 'https://mysterious-stream-02042-46f5840c9040.herokuapp.com/'
 
 export default domain
