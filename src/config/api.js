c4f18cdc0868d55852da73557def7e53a6b589ad
Solly <solobarine@gmail.com>
2024-01-06 08:28:42 +0100

    Add App Config

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,6 @@
-const status = 'production'
+const status = 'development'
 
-const domain = (status === 'development') ? 'http://localhost:8000/api' : 'https://mysterious-stream-02042-46f5840c9040.herokuapp.com/api'
+const domain =
+  status === 'development' ? 'http://localhost:8000/api' : 'https://mysterious-stream-02042-46f5840c9040.herokuapp.com/api'
 
 export default domain
