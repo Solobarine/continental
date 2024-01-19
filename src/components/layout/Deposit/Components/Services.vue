7b5f99a8c1819cddadc97d7a0b7529f204d85846
solly <solobarine@gmail.com>
2024-01-19 21:24:40 +0100

    Add Services Component.

---
+++ src/components/layout/Deposit/Components/Services.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Service from './Service.vue'
+
+const emit = defineEmits()
+const props = defineProps(['services', 'payload'])
+</script>
+
+<template>
+  <p id="serviceCall">Select Preferred Payment Platform</p>
+  <div class="service_options">
+    <Service
+      @click="emit('selectService', service.name)"
+      v-for="(service, i) in props.services"
+      :key="i"
+      :name="service.name"
+      :class="service.name === props.payload.service ? 'highlight' : ''"
+    >
+      <component :is="service.logo"></component>
+    </Service>
+  </div>
+</template>
+<style>
+#serviceCall {
+  text-align: center;
+  padding-bottom: 1.2em;
+}
+</style>
