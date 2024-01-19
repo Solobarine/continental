1a8241d3a04d97ce6a5282d93f037fd47ca1ce2d
solly <solobarine@gmail.com>
2024-01-19 21:14:51 +0100

    Refresh Dashboard Design.

--- src/components/layout/Dashboard.vue
+++ src/components/layout/Dashboard.vue
@@ -41,11 +41,16 @@
         </div>
       </div>
     </div>
-    <div id="wallet">
+    <div id="wallet" :class="{ active: showWallet }">
+      <ion-icon
+        @click="toggleWallet"
+        onblur="toggleWallet"
+        name="grid"
+        class="toggle_wallet"
+        :class="{ active: showWallet }"
+      ></ion-icon>
       <p class="quick">My Wallet</p>
-      <Card
-        :first_name="user.first_name"
-        :last_name="user.last_name" />
+      <Card :first_name="user.first_name" :last_name="user.last_name" />
       <div id="quick_transfer">
         <p class="quick">Quick Transfer</p>
         <form>
@@ -54,28 +59,26 @@
             name="amount"
             id="amount"
             v-model="payload.amount"
-            placeholder="Enter Amount" />
+            placeholder="Enter Amount"
+          />
           <input
             type="number"
             name="receiver"
             id="receiver"
             v-model="account_no"
             placeholder="Receiver Account Number"
-            @input="getReceiver" />
-          <p
-            v-if="error"
-            id="error">
+            @input="getReceiver"
+          />
+          <p v-if="error" id="error">
             {{ error }}
           </p>
-          <p
-            v-if="success"
-            id="success">
+          <p v-if="success" id="success">
             {{ success.first_name }} {{ success.last_name }}
           </p>
           <input
             :disabled="!payload.payee_id"
             @click="
-              (e) => {
+              e => {
                 e.preventDefault()
                 console.log('click')
 
@@ -84,7 +87,8 @@
               }
             "
             type="submit"
-            value="Send" />
+            value="Send"
+          />
         </form>
       </div>
     </div>
@@ -99,20 +103,27 @@ import { useUserStore } from '../../stores/UserStore'
 import { useTransferStore } from '../../stores/TransferStore'
 import { ref } from 'vue'
 
-const userStore = useUserStore()
-const user = userStore.user
+await useUserStore().getUser()
 
+const userStore = useUserStore()
+const { user, transactions } = userStore
+console.log(transactions)
 const account_no = ref('')
 let error = ref('')
 let success = ref('')
 let value = ref(null)
+const showWallet = ref(false)
+
+const toggleWallet = () => {
+  showWallet.value = !showWallet.value
+}
 
 const payload = {
   amount: '',
   account_number: '',
   user_id: user.id,
   payee_id: '',
-  description: ''
+  description: '',
 }
 
 const getReceiver = async () => {
@@ -121,7 +132,7 @@ const getReceiver = async () => {
     error.value = ''
     success.value = {}
     value = await useUserStore().getReceiver({
-      account_number: account_no.value
+      account_number: account_no.value,
     })
     if (value.status === 200) {
       success.value = value.data
@@ -149,22 +160,25 @@ const getReceiver = async () => {
   fill: var(--white);
 }
 
+#dash {
+  flex-grow: 1;
+}
+
 h1 {
   margin: 20px 0;
   font-size: 20px;
 }
 
 #details {
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(12em, 1fr));
+  padding: 1em;
   gap: 30px;
   flex-wrap: wrap;
 }
 
 .account_details {
   padding: 20px 10px;
-  width: 220px;
-  min-width: 80px;
-  /* border: 1px solid var(--text); */
   border-radius: 15px;
   background-color: var(--faint);
 }
@@ -173,11 +187,15 @@ h1 {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 20px;
+  margin-bottom: 5px;
 }
 
 .account_details > p {
   font-size: 18px;
+}
+
+.account_details > p:last-child {
+  font-weight: 500;
 }
 
 #recents_section {
@@ -189,9 +207,9 @@ h1 {
 }
 
 #recents {
-  display: flex;
+  display: grid;
   gap: 30px;
-  flex-wrap: wrap;
+  grid-template-columns: repeat(auto-fit, minmax(20em, 1fr));
 }
 
 #recent_transfers,
@@ -223,8 +241,8 @@ h1 {
 }
 
 #wallet {
+  position: relative;
   padding: 10px;
-  padding-top: 30px;
   flex-basis: 300px;
   border-top-left-radius: 20px;
   border-bottom-left-radius: 20px;
@@ -272,6 +290,10 @@ input[type='submit']:disabled {
   opacity: 0.7;
 }
 
+.toggle_wallet {
+  display: none;
+}
+
 @media screen and (max-width: 750px) {
   #dashboard {
     flex-wrap: wrap;
@@ -282,9 +304,35 @@ input[type='submit']:disabled {
   }
 
   #wallet {
-    flex-grow: 1;
+    min-width: 15em;
+    position: fixed;
+    right: -18em;
+    top: 60px;
+    height: 100dvh;
     border-radius: 20px;
     margin-bottom: 15px;
+    transition: 500ms ease-in;
+  }
+
+  #wallet.active {
+    right: 0;
+  }
+
+  .toggle_wallet {
+    display: block;
+    position: fixed;
+    right: 0.4em;
+    color: var(--primary);
+    font-size: 2em;
+    visibility: visible;
+    transition: 500ms ease-in;
+  }
+
+  .toggle_wallet.active {
+    position: absolute;
+    color: var(--faint);
+    right: 0.4em;
+    z-index: 30;
   }
 
   .quick {
