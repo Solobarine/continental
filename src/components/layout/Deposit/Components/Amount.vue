27145d6879fefc1f34007466c3360474f0c3bee7
solly <solobarine@gmail.com>
2024-01-19 21:23:30 +0100

    Add Amount Component.

---
+++ src/components/layout/Deposit/Components/Amount.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="amount_option">
+    <div>
+      <ion-icon name="wallet" class="wallet"></ion-icon>
+      <div class="content">
+        <span>$</span>
+        <p>{{ data.amount }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const data = defineProps(['amount', 'payload', 'selected'])
+const emit = defineEmits()
+</script>
