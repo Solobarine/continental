f479586062cd6d49056f6e667e4dc1f4a4c37fe6
solly <solobarine@gmail.com>
2024-01-19 21:19:32 +0100

    Refresh Deposit Layout and Design.

--- src/components/layout/Deposit/Deposit.vue
+++ src/components/layout/Deposit/Deposit.vue
@@ -1,159 +1,18 @@
 <template>
   <section id="deposit">
-    <!-- <h3>Deposit</h3> -->
-    <div id="services">
-      <h4>Choose a Service</h4>
-      <div id="services_list">
-        <div
-          @click="select_deposit_option(one)"
-          class="service"
-          name="Visa">
-          <Check
-            ref="one"
-            class="check" />
-          <Visa />
-          <p>Visa</p>
-        </div>
-
-        <div
-          @click="select_deposit_option(two)"
-          class="service"
-          name="MasterCard">
-          <Check
-            ref="two"
-            class="check" />
-          <MasterCard />
-          <p>MasterCard</p>
-        </div>
-        <div
-          @click="select_deposit_option(three)"
-          class="service"
-          name="Maestro">
-          <Check
-            ref="three"
-            class="check" />
-          <Maestro />
-          <p>Maestro</p>
-        </div>
-        <div
-          @click="select_deposit_option(four)"
-          class="service"
-          name="American Express">
-          <Check
-            ref="four"
-            class="check" />
-          <AmericanExpress />
-          <p>American Express</p>
-        </div>
-        <div
-          @click="select_deposit_option(five)"
-          class="service"
-          name="Discovery">
-          <Check
-            ref="five"
-            class="check" />
-          <Discovery />
-          <p>Discovery</p>
-        </div>
-        <div
-          @click="select_deposit_option(six)"
-          class="service"
-          name="JCB">
-          <Check
-            ref="six"
-            class="check" />
-          <JCB />
-          <p>JCB</p>
-        </div>
-        <div
-          @click="select_deposit_option(seven)"
-          class="service"
-          name="PayPal">
-          <Check
-            ref="seven"
-            class="check" />
-          <PayPal />
-          <p>PayPal</p>
-        </div>
-        <div
-          @click="select_deposit_option(eight)"
-          class="service"
-          name="UnionPay">
-          <Check
-            ref="eight"
-            class="check" />
-          <UnionPay />
-          <p>UnionPay</p>
-        </div>
-      </div>
-    </div>
-    <div id="amount_section">
-      <h4>Amount</h4>
-      <div id="amount_options">
-        <div
-          @click="select_amount(amount_one, false)"
-          class="amount_option"
-          name="3000">
-          <Check
-            ref="amount_one"
-            class="amount_check" />
-          <p>$3000</p>
-        </div>
-        <div
-          @click="select_amount(amount_two, false)"
-          class="amount_option"
-          name="1000">
-          <Check
-            ref="amount_two"
-            class="amount_check" />
-          <p>$1000</p>
-        </div>
-        <div
-          @click="select_amount(amount_three, false)"
-          class="amount_option"
-          name="500">
-          <Check
-            ref="amount_three"
-            class="amount_check" />
-          <p>$500</p>
-        </div>
-        <div
-          @click="select_amount(amount_four, false)"
-          class="amount_option"
-          name="100">
-          <Check
-            ref="amount_four"
-            class="amount_check" />
-          <p>$100</p>
-        </div>
-        <div
-          @click="select_amount(other, true)"
-          class="amount_option"
-          name="other">
-          <Check
-            ref="other"
-            class="amount_check" />
-          <p>Other</p>
-        </div>
-      </div>
-      <div id="options">
-        <input
-          v-if="selected_amount.value"
-          v-model="payload.amount"
-          type="number"
-          name="amount"
-          autocomplete="amount"
-          placeholder="Enter Amount" />
-        <input
-          @click="useDepositStore().makeDeposit(payload)"
-          type="submit"
-          value="Proceed" />
-      </div>
-    </div>
+    <h2>Add Credit</h2>
+    <Form
+      @depositRequest="data => makeDeposit(data)"
+      :steps="[Amounts, Services, Summary]"
+      :amounts="amounts"
+      :services="services"
+      :payload="payload"
+      :deposit-value="payload.amount"
+    />
   </section>
 </template>
 <script setup>
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 import AmericanExpress from '../../icons/services/AmericanExpress.vue'
 import Discovery from '../../icons/services/Discovery.vue'
 import JCB from '../../icons/services/JCB.vue'
@@ -162,126 +21,119 @@ import MasterCard from '../../icons/services/MasterCard.vue'
 import PayPal from '../../icons/services/PayPal.vue'
 import UnionPay from '../../icons/services/UnionPay.vue'
 import Visa from '../../icons/services/Visa.vue'
-import Check from '../../icons/Check.vue'
+import Services from './Components/Services.vue'
 import { useUserStore } from '../../../stores/UserStore'
 import { useDepositStore } from '../../../stores/DepositStore'
+import Form from './Components/Form.vue'
+import Amounts from './Components/Amounts.vue'
+import Summary from './Components/Summary.vue'
 
-const payload = {
+const amounts = [2000, 1000, 500, 200, 100, 50, 20, 10]
+
+const services = [
+  {
+    name: 'AmericanExpress',
+    logo: AmericanExpress,
+  },
+  {
+    name: 'Discovery',
+    logo: Discovery,
+  },
+  {
+    name: 'JCB',
+    logo: JCB,
+  },
+  {
+    name: 'Maestro',
+    logo: Maestro,
+  },
+  {
+    name: 'MasterCard',
+    logo: MasterCard,
+  },
+  {
+    name: 'Visa',
+    logo: Visa,
+  },
+  {
+    name: 'PayPal',
+    logo: PayPal,
+  },
+  {
+    name: 'UnionPay',
+    logo: UnionPay,
+  },
+]
+
+const payload = ref({
   service: '',
   amount: 0,
-  user_id: useUserStore().user.id
-}
+  user_id: useUserStore().user.id,
+})
 
-console.log(payload)
+const makeDeposit = data => {
+  payload.value.amount = data.amount
+  payload.value.service = data.service
+  payload.value.user_id = useUserStore().user.id
 
-const selected_amount = reactive({ value: false })
-
-const one = ref(null)
-const two = ref(null)
-const three = ref(null)
-const four = ref(null)
-const five = ref(null)
-const six = ref(null)
-const seven = ref(null)
-const eight = ref(null)
-
-const amount_one = ref(null)
-const amount_two = ref(null)
-const amount_three = ref(null)
-const amount_four = ref(null)
-const other = ref(null)
-
-const select_deposit_option = (number) => {
-  const checks = Array.from(document.querySelectorAll('.check'))
-  checks.map((check) => check.classList.remove('selected'))
-  number.$el.classList.add('selected')
-  payload.service = number.$el.parentElement.getAttribute('name')
-  console.log(payload)
-}
-
-const select_amount = (number, bool) => {
-  selected_amount.value = bool
-  const amounts = Array.from(document.querySelectorAll('.amount_check'))
-  amounts.map((amount) => amount.classList.remove('selected'))
-  number.$el.classList.add('selected')
-  payload.amount = parseFloat(number.$el.parentElement.getAttribute('name'))
-  console.log(payload)
+  console.log(payload.value)
+  useDepositStore().makeDeposit(payload.value)
 }
 </script>
-<style scoped>
+<style>
 #deposit {
-  max-width: 800px;
+  max-width: 45em;
   margin: auto;
+  padding: 0.2em;
 }
 
-#services h4,
-#amount_section h4,
-#or {
-  margin: 20px 0;
+#deposit > h2 {
+  text-align: center;
 }
 
-#services_list {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-}
-.service {
+#depositForm {
   position: relative;
-  width: 150px;
-  backdrop-filter: blur(4px) saturate(149%);
-  -webkit-backdrop-filter: blur(4px) saturate(149%);
-  background-color: rgba(100, 100, 100, 0.15);
+  max-width: 45em;
+  margin: auto;
+  width: 100%;
+  border-radius: 0.5em;
+  padding: 1em 0;
+  margin-top: 1.5em;
+  background-color: var(--faint);
+  box-shadow: var(--box-shadow);
+}
+
+#depositForm #controls {
+  position: absolute;
+  bottom: 1em;
+  right: 1em;
   display: flex;
-  flex-direction: column;
-  border-radius: 15px;
-  padding: 10px 0;
   align-items: center;
+  gap: 0.5em;
+}
+
+#depositForm #controls > button {
+  background-color: var(--primary);
+  color: var(--faint);
+  padding: 0.75em 1.2em;
+  border-radius: 0.5em;
+  font-size: 0.9em;
+  outline: none;
+  border: none;
   cursor: pointer;
 }
-.service svg {
-  height: 50px;
-  display: block;
-  background-size: cover;
-}
 
-.service .check,
-#amount_options div .amount_check {
-  height: 20px;
-  fill: blue;
-  position: absolute;
-  top: 5px;
-  right: 10px;
-  scale: 0;
-  opacity: 0;
-}
-
-.service .selected,
-#amount_options div .selected {
-  opacity: 1;
-  scale: 1;
-}
-
-#amount_options {
+.service_options,
+.amount_options {
+  position: relative;
   display: flex;
+  align-items: center;
+  justify-content: center;
   gap: 20px;
+  margin: auto;
   flex-wrap: wrap;
   margin-bottom: 20px;
-}
-
-.amount_option {
-  padding: 20px 10px;
-  padding-right: 60px;
-  backdrop-filter: blur(4px) saturate(149%);
-  -webkit-backdrop-filter: blur(4px) saturate(149%);
-  background-color: rgba(100, 100, 100, 0.15);
-  border-radius: 12px;
-  cursor: pointer;
-}
-
-.amount_option p {
-  font-size: 17px;
-  text-align: center;
-  margin: auto;
+  padding-bottom: 6em;
 }
 
 #amount_section input[type='number'] {
@@ -289,17 +141,32 @@ const select_amount = (number, bool) => {
   border-radius: 10px;
   border: 1px solid var(--text);
   margin-bottom: 15px;
+  border: 1px solid var(--primary);
+  outline: none;
   display: block;
 }
 
 input[type='submit'] {
   display: block;
   cursor: pointer;
+  margin-top: 1em;
+  background-color: var(--primary);
+  border-radius: 0.5em;
+  padding: 1em;
+  color: var(--faint);
+  outline: none;
+  border: none;
 }
 
-#options {
+#deposit #options {
   max-width: 220px;
   display: flex;
   flex-direction: column;
+}
+
+@media screen and (max-width: 650px) {
+  #deposit {
+    padding-top: 2em;
+  }
 }
 </style>
