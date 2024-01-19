4b7dfb3b07efee0408106e413ab6197f8cdff512
solly <solobarine@gmail.com>
2024-01-19 21:17:19 +0100

    Modify Store

--- src/stores/DepositStore.js
+++ src/stores/DepositStore.js
@@ -1,64 +1,67 @@
-import { defineStore } from "pinia";
-import domain from "../config/api";
-import postData from "../utils/postData";
-import getData from "../utils/getData";
-import { useUserStore } from "./UserStore";
+import { defineStore } from 'pinia'
+import domain from '../config/api'
+import postData from '../utils/postData'
+import getData from '../utils/getData'
+import { useUserStore } from './UserStore'
+import { router } from '../router/router'
 
 const useDepositStore = defineStore('deposit', {
-    state: () =>({
-        deposits: [],
-        loading: Boolean(false),
-        errror: String(''),
-        message: String('')
-    }),
-    getters: {
-        successful() {
-            return this.messages.filter(message => message.status === 'completed')
-        },
-        failed() {
-            return this.messages.filter(message => message.status === 'failed')
-        }
+  state: () => ({
+    deposits: [],
+    loading: Boolean(false),
+    errror: String(''),
+    message: String(''),
+  }),
+  getters: {
+    successful() {
+      return this.messages.filter(message => message.status === 'completed')
     },
-    actions: {
-        async getDeposits() {
-            this.loading = true
-            const url = `${domain}/account/deposits`
-            const token = `Bearer ${localStorage.getItem('auth_token')}`
-            const deposits = await getData(url, token)
-            console.log(deposits);
-            this.loading = false
-            if (deposits.status !== 200) {
-                this.error = deposits.response.data.message
-                return
-            }
-            
-            this.deposits = deposits.data.deposits
-            this.message = deposits.data.message
-            this.error = ''
-            return
-        },
-        async makeDeposit(payload) {
-            this.loading = true
-            const url = `${domain}/account/deposits`
-            const token = `Bearer ${localStorage.getItem('auth_token')}`
-            const deposit = await postData(url, payload, token)
-            console.log(deposit);
-            this.loading = false
-            if (deposit.status !== 201) {
-                this.error = deposit.response.data.message
-                return
-            }
+    failed() {
+      return this.messages.filter(message => message.status === 'failed')
+    },
+  },
+  actions: {
+    async getDeposits() {
+      this.loading = true
+      const url = `${domain}/account/deposits`
+      const token = `Bearer ${localStorage.getItem('auth_token')}`
+      const deposits = await getData(url, token)
+      console.log(deposits)
+      this.loading = false
+      if (deposits.status !== 200) {
+        this.error = deposits.response.data.message
+        return
+      }
 
-            this.deposits.push(deposit.data)
-            console.log(useUserStore().user.balance);
-            useUserStore().updateBalanceDeposit(deposit.data.amount)
-            console.log(useUserStore().user.balance);
-            this.message = deposit.data.message
-            this.error = ''
+      this.deposits = deposits.data.deposits
+      this.message = deposits.data.message
+      this.error = ''
+      return
+    },
+    async makeDeposit(payload) {
+      this.loading = true
+      const url = `${domain}/account/deposits`
+      const token = `Bearer ${localStorage.getItem('auth_token')}`
+      const deposit = await postData(url, payload, token)
+      console.log(deposit)
+      this.loading = false
+      if (deposit.status !== 201) {
+        this.error = deposit.response.data.message
+        return
+      }
 
-            return
-        }
-    }
+      this.deposits.push(deposit.data)
+      console.log(useUserStore().user.balance)
+      useUserStore().updateBalanceDeposit(deposit.data.amount)
+      console.log(useUserStore().user.balance)
+      this.message = deposit.data.message
+      this.error = ''
+      router.replace('/dashboard')
+
+      return
+    },
+  },
 })
 
 export { useDepositStore }
+
--- src/stores/TransferStore.js
+++ src/stores/TransferStore.js
@@ -1,59 +1,61 @@
-import { defineStore } from "pinia";
-import domain from "../config/api";
-import postData from "../utils/postData";
-import getData from "../utils/getData";
-import { useUserStore } from "./UserStore";
+import { defineStore } from 'pinia'
+import domain from '../config/api'
+import postData from '../utils/postData'
+import getData from '../utils/getData'
+import { useUserStore } from './UserStore'
+import { router } from '../router/router'
 
 const useTransferStore = defineStore('transfer', {
-    state: () =>({
-        transfers: [],
-        loading: Boolean(false),
-        errror: String(''),
-        success: ''
-    }),
-    getters: {
-        successful() {
-            return this.transfers.filter(transfer => transfer.status === 'completed')
-        },
-        failed() {
-            return this.transfers.filter(transfer => transfer.status === 'failed')
-        }
+  state: () => ({
+    transfers: [],
+    loading: Boolean(false),
+    errror: String(''),
+    success: '',
+  }),
+  getters: {
+    successful() {
+      return this.transfers.filter(transfer => transfer.status === 'completed')
     },
-    actions: {
-        async getTransfers() {
-            this.loading = true
-            const url = `${domain}/account/transfers`
-            const token = `Bearer ${localStorage.getItem('auth_token')}`
-            const transfers = await getData(url, token)
-            this.loading = false
-            if (transfers.status !== 201) {
-                this.error = transfers.response.data.message
-                return
-            }
-            
-            this.transfers = transfers.data.transfers
-            this.error = ''
-            return
-        },
-        async makeTransfer(payload) {
-            this.loading = true
-            const url = `${domain}/account/transfers`
-            const token = `Bearer ${localStorage.getItem('auth_token')}`
-            const transfer = await postData(url, payload, token)
-            // console.log(transfer);
-            this.loading = false
-            if (transfer && transfer.status !== 201) {
-                this.error = transfer.response.data.message
-                return {error: this.error}
-            }
-            this.transfers.push(transfer.data.transfer)
-            this.success = 'Transfer Successful'
-            // console.log(this.success);
-            this.error = ''
-            useUserStore().updateBalanceTransfer(transfer.data.amount)
-            return {success: this.success}
-        }
-    }
+    failed() {
+      return this.transfers.filter(transfer => transfer.status === 'failed')
+    },
+  },
+  actions: {
+    async getTransfers() {
+      this.loading = true
+      const url = `${domain}/account/transfers`
+      const token = `Bearer ${localStorage.getItem('auth_token')}`
+      const transfers = await getData(url, token)
+      this.loading = false
+      if (transfers.status !== 201) {
+        this.error = transfers.response.data.message
+        return
+      }
+
+      this.transfers = transfers.data.transfers
+      this.error = ''
+      return
+    },
+    async makeTransfer(payload) {
+      this.loading = true
+      const url = `${domain}/account/transfers`
+      const token = `Bearer ${localStorage.getItem('auth_token')}`
+      const transfer = await postData(url, payload, token)
+      console.log(transfer)
+      this.loading = false
+      if (transfer && transfer.status !== 201) {
+        this.error = transfer.response.data.message
+        return { error: this.error }
+      }
+      this.transfers.push(transfer.data.transfer)
+      this.success = 'Transfer Successful'
+      this.error = ''
+      useUserStore().updateBalanceTransfer(transfer.data.amount)
+      router.push('/dashboard')
+      return { success: this.success }
+    },
+  },
 })
 
 export { useTransferStore }
+
