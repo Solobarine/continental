fb8f32d8e081fd3200a9902936f643d577234623
solly <solobarine@gmail.com>
2024-01-26 22:35:31 +0100

    Create Appliction Component

---
+++ src/components/layout/Application.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import { useUserStore } from '../../stores/UserStore'
+
+const userStore = useUserStore()
+if (!userStore.is_logged_in) userStore.getUser()
+
+console.log(useUserStore().user)
+</script>
+
+<template>
+  <Header />
+  <router-view></router-view>
+  <Footer />
+</template>
